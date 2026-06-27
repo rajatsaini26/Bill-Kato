78a171dbccf480ccceda7afe6a011e3dd95dd933
rajatsaini26 <rajattak7@gmail.com>
2026-06-27 23:07:56 +0530

    updated the Google Drive permissions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import '../db/client';
 
 GoogleSignin.configure({
-  scopes: ['https://www.googleapis.com/auth/drive.appdata'],
+  scopes: ['https://www.googleapis.com/auth/drive.file'],
   webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
 });
 
